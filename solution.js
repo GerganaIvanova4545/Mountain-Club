4c53606ab90cd04cfc487f01c5209f57ee82f584
Gergana Ivanova <gerganka.ivanova@gmail.com>
2024-02-09 11:56:34 +0200

    Html, js, css

--- solution.js
+++ solution.js
@@ -1,1 +1,7 @@
-
+document.getElementById('register').addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelectorAll('.page').forEach(x => x.classList.add('hide'));
+    document.querySelectorAll('.nav ul li a').forEach(x => x.classList.remove('new-color'));
+    e.target.classList.add('new-color');
+    document.querySelector('.register-page').classList.remove('hide');
+});
